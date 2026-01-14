9177e0759212631d8ee475bd2a7b9582f226e125
servadmin <servadmin@ubuntu>
2026-01-13 21:18:56 -0500

    Update graphics Y position

--- demo/src/main/java/com/codeferm/all/demo/LedDisplay.java
+++ demo/src/main/java/com/codeferm/all/demo/LedDisplay.java
@@ -72,7 +72,7 @@ public class LedDisplay extends Base {
         // Calculate dynamic center positions using base class width and height
         var centerX = (getWidth() - 16) / 2;
         // Lift sprite slightly for text space
-        var centerY = (getHeight() / 2) - 14;
+        var centerY = (getHeight() / 2) - 16;
         // Rough estimate for 6-pixel font width
         var textX = (getWidth() - (text.length() * 6)) / 2;
         var textY = centerY + 24;
